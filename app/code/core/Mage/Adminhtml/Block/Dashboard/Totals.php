ea9b30198b5ab094933e1d7ef1f1648ae97c7c85
Thillai Prabu <prabusmiles05@gmail.com>
2018-04-23 11:45:22 +0530

    Dashboard changes tmp

--- app/code/core/Mage/Adminhtml/Block/Dashboard/Totals.php
+++ app/code/core/Mage/Adminhtml/Block/Dashboard/Totals.php
@@ -42,6 +42,7 @@ class Mage_Adminhtml_Block_Dashboard_Totals extends Mage_Adminhtml_Block_Dashboa
 
     protected function _prepareLayout()
     {
+        $debug = true;
         if (!Mage::helper('core')->isModuleEnabled('Mage_Reports')) {
             return $this;
         }
@@ -75,5 +76,27 @@ class Mage_Adminhtml_Block_Dashboard_Totals extends Mage_Adminhtml_Block_Dashboa
         $this->addTotal($this->__('Tax'), $totals->getTax());
         $this->addTotal($this->__('Shipping'), $totals->getShipping());
         $this->addTotal($this->__('Quantity'), $totals->getQuantity()*1, true);
+    }
+
+    public function getOrderCount($status)
+    {
+        if (!Mage::helper('core')->isModuleEnabled('Mage_Reports')) {
+            return $this;
+        }
+        $isFilter = $this->getRequest()->getParam('store') || $this->getRequest()->getParam('website') || $this->getRequest()->getParam('group');
+        $period = $this->getRequest()->getParam('period', '24h');
+
+        /* @var $collection Mage_Reports_Model_Mysql4_Order_Collection */
+        $collection = Mage::getResourceModel('reports/order_collection')
+            ->addCreateAtPeriodFilter($period)
+        ;
+
+        $collection->getSelect()->where(
+            'status = ?', $status
+        );
+
+        $str = (string)$collection->getSelect();
+
+        return $collection->getSize();
     }
 }
