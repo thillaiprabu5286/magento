c7da77b5ad2db9bc26de0a53a8359cd57eb1819c
Thillai Prabu <prabusmiles05@gmail.com>
2018-05-05 10:07:28 +0530

    Existing customer api

--- app/code/local/Ignovate/Mobile/Model/Api2/Customer/Rest/Admin/V2.php
+++ app/code/local/Ignovate/Mobile/Model/Api2/Customer/Rest/Admin/V2.php
@@ -6,6 +6,7 @@ class Ignovate_Mobile_Model_Api2_Customer_Rest_Admin_V2
 
     public function _create($request)
     {
+        $debug = true;
         try {
 
             // Validate if consumer key is set in request and if it exists
@@ -18,49 +19,61 @@ class Ignovate_Mobile_Model_Api2_Customer_Rest_Admin_V2
                 Mage::throwException('Consumer key is incorrect');
             }
 
-            // Initialize empty customer model
-            $customer = Mage::getModel('customer/customer')
-                ->setWebsiteId(self::DEFAULT_CITY)
-                ->setStoreId(self::DEFAULT_STORE)
-                ->setId(null);
-            // Initialize customer group
-            $customer->getGroupId();
-
             if ($request['source'] == 'direct') {
                 $email = $request['key'] . '@veggies.com';
             } else {
                 $email = $request['key'];
             }
-            $customer->setEmail($email);
-            $customer->setPassword($request['key']);
-            $customer->setPasswordConfirmation($request['key']);
-
-            // NOTE: preset of first and last name is temporal
-            if (!$customer->getFirstname()) {
-                $customer->setFirstname('FirstName');
-            }
-            if (!$customer->getLastname()) {
-                $customer->setLastname('LastName');
-            }
-
-            // Validate customer model
-            $customerErrors = $customer->validate();
-            if (true !== $customerErrors) {
-                foreach ($customerErrors as $error) {
-                    $this->_error($error, Mage_Api2_Model_Server::HTTP_OK);
-                }
-                Mage::throwException('Customer data is invalid');
-            }
-
-            // Save customer
-            $customer->save();
 
             // Generate token for new created customer
             /** @var Ignovate_Oauth_Model_Token $token */
             $token = Mage::getModel('oauth/token');
             $token->setConsumerId($consumer->getId());
-            $token->createFromCustomer($customer);
-            $token->save();
+
+            $customer = Mage::getModel('customer/customer');
+            $customer->setWebsiteId(self::DEFAULT_CITY)
+                ->setStoreId(self::DEFAULT_STORE);
+            $customer->loadByEmail($email);
+            if (is_object($customer) && $customer->getId()) {
+                $token->loadByCustomer($customer);
+            } else {
+                // Initialize empty customer model
+                $newCustomer = Mage::getModel('customer/customer')
+                    ->setWebsiteId(self::DEFAULT_CITY)
+                    ->setStoreId(self::DEFAULT_STORE)
+                    ->setId(null);
+
+                // Initialize customer group
+                $newCustomer->getGroupId();
+                $newCustomer->setEmail($email);
+                $newCustomer->setPassword($request['key']);
+                $newCustomer->setPasswordConfirmation($request['key']);
+
+                // NOTE: preset of first and last name is temporal
+                if (!$newCustomer->getFirstname()) {
+                    $newCustomer->setFirstname('FirstName');
+                }
+                if (!$newCustomer->getLastname()) {
+                    $newCustomer->setLastname('LastName');
+                }
+
+                // Validate customer model
+                $customerErrors = $newCustomer->validate();
+                if (true !== $customerErrors) {
+                    foreach ($customerErrors as $error) {
+                        $this->_error($error, Mage_Api2_Model_Server::HTTP_OK);
+                    }
+                    Mage::throwException('Customer data is invalid');
+                }
+
+                // Save customer
+                $newCustomer->save();
+
+                $customer = $newCustomer;
+
+                $token->createFromCustomer($customer);
+                $token->save();
+            }
 
             return array(
                 'token_id'          => $token->getEntityId(),
@@ -68,6 +81,7 @@ class Ignovate_Mobile_Model_Api2_Customer_Rest_Admin_V2
                 'token'             => $token->getToken(),
                 'secret'            => $token->getSecret(),
             );
+
         } catch (Exception $e) {
             // Catch any type of exception and convert it into API2 exception
             throw new Mage_Api2_Exception(
