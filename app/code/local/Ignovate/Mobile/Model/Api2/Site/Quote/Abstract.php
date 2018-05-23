df946f848bf671d6a80024708fb986dc334d632b
Thillai Prabu <prabusmiles05@gmail.com>
2018-05-23 11:10:35 +0530

    store active quote api

---
+++ app/code/local/Ignovate/Mobile/Model/Api2/Site/Quote/Abstract.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prabu
+ * Date: 08/11/17
+ * Time: 12:41 PM
+ */
+class Ignovate_Mobile_Model_Api2_Site_Quote_Abstract extends Ignovate_Api2_Model_Resource
+{
+}
---
+++ app/code/local/Ignovate/Mobile/Model/Api2/Site/Quote/Rest/Admin/V2.php
@@ -0,0 +1,29 @@
+<?php
+
+class Ignovate_Mobile_Model_Api2_Site_Quote_Rest_Admin_V2
+    extends Ignovate_Mobile_Model_Api2_Site_Quote_Abstract
+{
+    public function _retrieve()
+    {
+        $storeId = $this->getRequest()->getParam('store_id');
+        $customerId = $this->getRequest()->getParam('customer_id');
+
+        if (empty($customerId)) {
+            $this->_critical(self::RESOURCE_DATA_INVALID);
+        }
+
+        //Get Sales quote by customer store
+        /** @var Mage_Sales_Model_Resource_Quote_Collection $collection */
+        $collection = Mage::getModel('sales/quote')->getCollection();
+        $collection->addFieldToFilter('store_id', $storeId)
+            ->addFieldToFilter('customer_id', $customerId)
+            ->addFieldToFilter('is_active', 1);
+
+        $str = (string)$collection->getSelect();
+
+        $data = $collection->getLastItem();
+
+        return array ('quote_id' => $data->getEntityId());
+    }
+
+}
