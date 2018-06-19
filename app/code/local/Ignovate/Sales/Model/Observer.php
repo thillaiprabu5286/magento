bc7f4724a8a557f161aa5c90e3a588eeeb21a949
Prabu Goodhope <prabu@goodhope.ae>
2018-06-19 08:48:23 +0530

    Modify Order status on save driver

--- app/code/local/Ignovate/Sales/Model/Observer.php
+++ app/code/local/Ignovate/Sales/Model/Observer.php
@@ -5,7 +5,7 @@ class Ignovate_Sales_Model_Observer
     // for event sales_order_save_commit_after
     public function triggerCompleteSms($observer)
     {
-        $order = $observer->getOrder();
+        $order = $observer->getEvent()->getOrder();
         if($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE){
             // send sms
             /** @var Ignovate_Sms_Helper_Fcm $helper */
@@ -14,5 +14,17 @@ class Ignovate_Sales_Model_Observer
         }
 
         return $this;
+    }
+
+    public function onSaveDriverChangeStatus($observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+        if($order->getState() == 'ready_for_delivery') {
+            if (!empty($order->getDriver()) && $order->getDriver() != '') {
+                //Modify order status
+                $order->setStatus('driver_assigned');
+            }
+        }
+
     }
 }
