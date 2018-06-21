086b22b2faf71dcb8f47527b13b177e25007af09
Prabu Goodhope <prabu@goodhope.ae>
2018-06-21 14:48:46 +0530

    New order sms trigger

--- app/code/local/Ignovate/Sales/Model/Observer.php
+++ app/code/local/Ignovate/Sales/Model/Observer.php
@@ -7,9 +7,16 @@ class Ignovate_Sales_Model_Observer
     {
         $order = $observer->getEvent()->getOrder();
 
-        /** @var Ignovate_Sms_Helper_Fcm $helper */
-        $helper = Mage::helper('ignovate_sms/fcm');
-        $helper->sendSms($order);
+        if ($order->getStatus() == 'pending') {
+            /** @var Ignovate_Sms_Helper_Data $helper */
+            $helper = Mage::helper('ignovate_sms');
+            $helper->sendSms($order, 'NewOrderNew');
+        } else {
+            /** @var Ignovate_Sms_Helper_Fcm $helper */
+            $helper = Mage::helper('ignovate_sms/fcm');
+            $helper->sendSms($order);
+
+        }
 
         return $this;
     }
